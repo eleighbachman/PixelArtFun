4e4b0e714b322a2b250043d97a5de4d674172e9f
Liz Bachman <eleighbachman@gmail.com>
2018-03-07 08:56:52 -0700

    first commit

---
+++ designs.js
@@ -0,0 +1,31 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+
+function makeGrid(h,w) {
+  console.log("Height is " + h + " and width is " + w + ".");
+  var tableGrid = document.getElementById("pixelCanvas");
+  for (var r=0; r<h; r++) {
+    var row = tableGrid.insertRow(r)
+    for (var c=0; c<w; c++) {
+      var column = row.insertCell(c);
+
+    };
+  };
+  $('td').addClass('selectable');
+  $('.selectable').click(function(event) {
+    var color = $('#colorPicker').val();
+    $(this).css('background-color', color);
+  })
+};
+
+$('#sizePicker').submit(function(event){
+  event.preventDefault();
+  var height = $('#inputHeight').val();
+  var width = $('#inputWeight').val();
+  alert("The button was clicked with " + height + " in height and "+ width + " in width!");
+  makeGrid(height, width);
+});
